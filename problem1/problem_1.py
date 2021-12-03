8bf1df9a8b898e6c4ebec725a451535e0e3cfc65
Alfred-N <alfredn@kth.se>
2021-12-03 12:43:50 +0100

    add files

---
+++ problem1/problem_1.py
@@ -0,0 +1,4 @@
+# Course: EL2805 - Reinforcement Learning - Lab 2 Problem 1
+# Code authors: [Alfred Nilsson - alfredn@kth.se][Alessio Galatolo - galatolo@kth.se]
+
+# The save file of the neural network needs to be named 'neural-network-1.pth'
