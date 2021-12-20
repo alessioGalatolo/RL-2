d38814567d57c97d40fec81bbb380b52dedec683
Alfred N <alfredn@kth.se>
2021-12-20 22:49:40 +0100

    add sweeps

--- problem1/DQN_problem.py
+++ problem1/DQN_problem.py
@@ -54,77 +54,59 @@ def legacy_model_loading(device):
 
 
 def main():
-    # parse arguments and options
-    parser = argparse.ArgumentParser(
-        description='Train a DQN agent in the Lunar Lander Environment')
-    parser.add_argument('--wandb',
-                        dest='WANDB',
-                        action='store_true',
-                        default=False)
-    parser.add_argument('--load-ckpt',
-                        dest='CKPT_PATH',
-                        default=None,
-                        help='Name of the checkpoint file')
-    parser.add_argument('--legacy-checkpoints',
-                        dest='LEGACY',
-                        action='store_true',
-                        default=False,
-                        help='Save network dicts instead of all model (use for retrocompatibility)')
-    parser.add_argument('--net',
-                        dest='NET',
-                        default='fully-connected',
-                        help='Network architecture: \'fully-connected\' or \'conv\' or \'simple-conv\'')
-    args = parser.parse_args()
+    # # parse arguments and options
+    # parser = argparse.ArgumentParser(
+    #     description='Train a DQN agent in the Lunar Lander Environment')
+    # parser.add_argument('--wandb',
+    #                     dest='WANDB',
+    #                     action='store_true',
+    #                     default=False)
+    # parser.add_argument('--load-ckpt',
+    #                     dest='CKPT_PATH',
+    #                     default=None,
+    #                     help='Name of the checkpoint file')
+    # parser.add_argument('--legacy-checkpoints',
+    #                     dest='LEGACY',
+    #                     action='store_true',
+    #                     default=False,
+    #                     help='Save network dicts instead of all model (use for retrocompatibility)')
+    # parser.add_argument('--net',
+    #                     dest='NET',
+    #                     default='fully-connected',
+    #                     help='Network architecture: \'fully-connected\' or \'conv\' or \'simple-conv\'')
+    # args = parser.parse_args()
 
-    # Parameters
-    replay_buffer_size = 10000  # set in range of 5000-30000
-    n_random_experiences = replay_buffer_size
-    batch_size_train = 64  # set in range 4-128
-    max_lr = 1e-3  # set in range 1e-3 to 1e-4
-    min_lr = 1e-4
-    CLIP_VAL = 1.5  # a value between 0.5 and 2
-    C_target = int(replay_buffer_size / batch_size_train)  # Target update frequency
-    start_episode = 0
-    N_episodes = 800  # set in range 100 to 1000
-    discount_factor = 0.8  # Value of the discount factor
-    n_ep_running_average = 50  # Running average of 50 episodes
-    eps_max = 0.99
-    eps_min = 0.05
-    decay_period = int(0.8 * N_episodes)
-    decay_method = 'linear'
-    LR_decay_period = int(0.9 * N_episodes)
-    checkpoint_interval = int(N_episodes / 20)
-    architecture = args.NET  # 'fully-connected' or 'conv' or 'simple-conv'
-    hidden_layers = [64, 64] if architecture == 'fully-connected' else None
-    loss_func = torch.nn.MSELoss()
-
-    if args.CKPT_PATH is not None:
-        start_episode = int(input('Enter starting episode (affects the exploration param eps): '))
-        n_random_experiences = 1
-        max_lr = 3e-4
-        eps_max = 0.1
-        LR_decay_period = int(0.9 * (N_episodes - start_episode))
-
-    # config for wandb
     config = dict(
-        replay_buffer_size=replay_buffer_size,  # set in range of 5000-30000
-        batch_size_train=batch_size_train,  # set in range 4-128
-        max_lr=max_lr,  # set in range 1e-3 to 1e-4
-        min_lr=min_lr,
-        CLIP_VAL=CLIP_VAL,  # a value between 0.5 and 2
-        C_target=C_target,  # Target update frequency
-        start_episode=start_episode,
-        N_episodes=N_episodes,  # set in range 100 to 1000
-        discount_factor=discount_factor,  # Value of the discount factor
-        n_ep_running_average=n_ep_running_average,  # Running average of 50 episodes
-        eps_max=eps_max,
-        eps_min=eps_min,
-        decay_period=decay_period,
-        decay_method=decay_method,
-        LR_decay_period=LR_decay_period,
-        hidden_layers=hidden_layers,
-        architecture=architecture
+        replay_buffer_size = 10000,  # set in range of 5000-30000
+        batch_size_train = 64,  # set in range 4-128
+        lr = 1e-3,  # set in range 1e-3 to 1e-4
+        CLIP_VAL = 1.5,  # a value between 0.5 and 2
+        N_episodes = 400,  # set in range 100 to 1000
+        discount_factor = 0.8,  # Value of the discount factor
+        eps_max = 0.99,
+        eps_min = 0.05,
+        decay_method = 'exponential',
+        n_hidden_l1 = 16,
+        n_hidden_l2 = 16,
+        architecture = 'fully-connected'  # 'fully-connected' or 'conv' or 'simple-conv'
     )
+    decay_period = int(0.8 * config['N_episodes'])
+    start_episode = 0
+    LR_decay_period = int(0.9 * config['N_episodes'])
+    n_random_experiences = config['replay_buffer_size']
+    n_ep_running_average = 50  # Running average of 50 episodes
+    C_target = int(config['replay_buffer_size'] / config['batch_size_train'])  # Target update frequency
+    checkpoint_interval = int(config['N_episodes'] / 20)
+    hidden_layers = [config['n_hidden_l1'], config['n_hidden_l2']] if config['architecture'] == 'fully-connected' else None
+    loss_func = torch.nn.MSELoss()
+    
+
+    # if args.CKPT_PATH is not None:
+    #     start_episode = int(input('Enter starting episode (affects the exploration param eps): '))
+    #     n_random_experiences = 1
+    #     max_lr = 3e-4
+    #     eps_max = 0.1
+    #     LR_decay_period = int(0.9 * (config['N_episodes'] - start_episode))
 
     # Check if GPU is available
     if torch.cuda.is_available():
@@ -141,14 +123,15 @@ def main():
     dim_state = len(env.observation_space.high)     # State dimensionality
 
     # Random agent initialization
-    agent = CleverAgent(n_actions, dim_state, device, eps_max, eps_min,
-                        decay_period=decay_period, decay_method=decay_method)
+    agent = CleverAgent(n_actions, dim_state, device, config['eps_max'], config['eps_min'],
+                        decay_period=decay_period, decay_method=config['decay_method'])
     random_agent = RandomAgent(n_actions)
 
     # Training process
-    replay_buffer = ReplayBuffer(replay_buffer_size)
+    replay_buffer = ReplayBuffer(config['replay_buffer_size'])
 
     # Initialize Q network
+    architecture = config['architecture']
     model_config = dict(d_in=dim_state,
                         hidden_layers=hidden_layers,
                         d_out=n_actions)
@@ -162,31 +145,34 @@ def main():
         print('Invalid network architecture, choose from \'fully-connected\' or \'conv\' or \'simple-conv\'')
         quit()
     q_network.to(device)
+    
 
     # Load pretrained weights
-    if args.CKPT_PATH is not None:
-        if args.LEGACY:
-            q_network.load_from_checkpoint(device, filename=args.CKPT_PATH)
-        else:
-            q_network = torch.load(args.CKPT_PATH).to(device)
+    # if args.CKPT_PATH is not None:
+    #     if args.LEGACY:
+    #         q_network.load_from_checkpoint(device, filename=args.CKPT_PATH)
+    #     else:
+    #         q_network = torch.load(args.CKPT_PATH).to(device)
 
     # Initialize target
     target_network = deepcopy(q_network)
     target_network.to(device)
 
     # Initialize WandB
-    mode = 'online' if args.WANDB else 'offline'
+    mode = 'online' #if args.WANDB else 'offline'
     wandb.init(project="Lab2", entity="el2805-rl", config=config, mode=mode)
-    run_name = wandb.run.name if args.WANDB else 'local'
+    run_name = wandb.run.name #if args.WANDB else 'local'
+    wandb.watch(q_network)
 
     # Initialize optimizers
-    optim_q = torch.optim.Adam(q_network.parameters(), lr=max_lr)
+    optim_q = torch.optim.Adam(q_network.parameters(), lr=config['lr'])
 
     # Initialize scheduler
-    scheduler = CosineAnnealingLR(optim_q, LR_decay_period, eta_min=min_lr)
+    scheduler = CosineAnnealingLR(optim_q, LR_decay_period, eta_min=config['lr'])
 
     # Track best model
     best_avg_reward = -np.inf
+    best_avg_episode = 0
 
     # -------------------------------------------------------------------------
     # ----------------- Fill replay buffer with random experiences ------------
@@ -207,11 +193,12 @@ def main():
     # trange is an alternative to range in python, from the tqdm library
     # It shows a nice progression bar that you can update with useful information
     # EPISODES = trange(N_episodes, desc='Episode: ', leave=True, initial=start_episode)
-    EPISODES = tqdm(range(start_episode, N_episodes, 1), desc='Episode: ', leave=True, initial=start_episode, total=N_episodes)
+    EPISODES = tqdm(range(start_episode, config['N_episodes'], 1), desc='Episode: ', leave=True, initial=start_episode, total=config['N_episodes'])
 
     # We will use these variables to compute the average episodic reward and
     # the average number of steps per episode
     episode_reward_list = []       # this list contains the total reward per episode
+    reward_running_avg_list = []
     episode_number_of_steps = []   # this list contains the number of steps per episode
     train_loss_list = []
     target_net_counter = 1
@@ -224,7 +211,7 @@ def main():
         state = env.reset()
         total_episode_reward = 0.
         t = 0
-        dummy_arr = np.arange(batch_size_train)
+        dummy_arr = np.arange(config['batch_size_train'])
         while not done:
             # Take a random action
             action = agent.forward(state, q_network)
@@ -237,7 +224,7 @@ def main():
             # Sample N experience from buffer and update q_net weights
 
             optim_q.zero_grad()
-            experience = replay_buffer.sample(min(batch_size_train, n_random_experiences))
+            experience = replay_buffer.sample(min(config['batch_size_train'], n_random_experiences))
             state_train, action_train, reward_train, next_state_train, done_train = experience
             action_train = np.array(list(map(lambda x: x.cpu().item(), action_train)),
                                     dtype=np.int32)
@@ -248,13 +235,13 @@ def main():
 
             with torch.no_grad():
                 target_qvals = agent.get_qvals(next_state_train, target_network)
-                target_val = reward_train + discount_factor * torch.max(target_qvals, dim=1).values * (1 - done_train)
+                target_val = reward_train + config['discount_factor'] * torch.max(target_qvals, dim=1).values * (1 - done_train)
 
             q_val = agent.get_qvals(state_train, q_network)[dummy_arr, action_train]
 
             train_loss = loss_func(q_val, target_val)
             train_loss.backward()
-            clip_grad_norm_(q_network.parameters(), CLIP_VAL)
+            clip_grad_norm_(q_network.parameters(), config['CLIP_VAL'])
             optim_q.step()
             detached_loss = train_loss.detach().cpu().numpy()
             train_loss_list.append(detached_loss)
@@ -286,22 +273,25 @@ def main():
             save_specs = dict(dir='checkpoints',
                               filename=f'ckpt_{i}_{run_name}',
                               date='')
-            if args.LEGACY:
-                q_network.save_checkpoint(**save_specs)
-            else:
-                q_network.save(**save_specs)
+            # if args.LEGACY:
+            #     q_network.save_checkpoint(**save_specs)
+            # else:
+            q_network.save(**save_specs)
 
         reward_running_avg = running_average(episode_reward_list, n_ep_running_average)[-1]
 
         # Save best model
         if reward_running_avg > best_avg_reward:
             best_avg_reward = reward_running_avg
+            best_avg_episode = i
             save_specs = dict(filename=f'best_{run_name}',
                               date='')
-            if args.LEGACY:
-                q_network.save_checkpoint(**save_specs)
-            else:
-                q_network.save(**save_specs)
+            # if args.LEGACY:
+                # q_network.save_checkpoint(**save_specs)
+            # else:
+            q_network.save(**save_specs)
+
+        reward_running_avg_list.append(reward_running_avg)
 
         # Updates the tqdm update bar with fresh information
         # (episode number, total reward of the last episode, total number of Steps
@@ -319,12 +309,13 @@ def main():
     save_specs = dict(dir='checkpoints',
                       filename=f'ckpt_{i}_{run_name}',
                       date='')
-    if args.LEGACY:
-        q_network.save_checkpoint(**save_specs)
-    else:
-        q_network.save(**save_specs)
+    # if args.LEGACY:
+    #     q_network.save_checkpoint(**save_specs)
+    # else:
+    q_network.save(**save_specs)
 
     # TODO: save plots for report
+    wandb.log({'max_avg_reward': max(reward_running_avg_list), 'best_avg_episode': best_avg_episode}, step=best_avg_episode)
 
     # Plot loss
     train_loss_list = np.array(train_loss_list)
@@ -335,6 +326,7 @@ def main():
     plt.savefig('loss.png')
 
     # Plot Rewards and steps
+    N_episodes = config['N_episodes']
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 9))
     ax[0].plot([i for i in range(1, (N_episodes - start_episode) + 1)],
                episode_reward_list, label='Episode reward')
